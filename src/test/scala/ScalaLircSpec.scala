5f70e715de13602139e56b1541e6f5547ee83a8d
Vlad Burca <vladburca2005@gmail.com>
2016-03-02 00:18:52 -0500

    Add tests

--- src/test/scala/ScalaLircSpec.scala
+++ src/test/scala/ScalaLircSpec.scala
@@ -4,16 +4,30 @@ import org.scalatest.FunSpec
 
 class ScalaLircSpec extends FunSpec {
   describe("ScalaLirc") {
-    val lirc = new ScalaLirc("src/test/scala/lircd.conf.test.valid")
+    val lirc = ScalaLirc("src/test/scala/lircd.conf.test.valid")
+
+    it ("should exist for a valid conf file") {
+      assert(lirc.isDefined)
+    }
+
+    it ("should graciously fail when conf file not present") {
+      val lircNotExists = ScalaLirc("src/test/scala/does.not.exist")
+      assert(!lircNotExists.isDefined)
+    }
+
+    it ("should graciously fail when conf file is not valid") {
+      val lircBadFile = ScalaLirc("src/test/scala/ScalaLircSpec.scala")
+      assert(!lircBadFile.isDefined)
+    }
 
     it ("should detect remotes") {
       val remotes = Set("TestRemote1", "TestRemote2")
-      assert(lirc.devices.toSet === remotes)
+      assert(lirc.get.devices.toSet === remotes)
     }
 
     it("should detect all the codes for a remote") {
-      assert(lirc.codesForRemote("TestRemote1").size == 4)
-      assert(lirc.codesForRemote("TestRemote1").contains("KEY_BRIGHTNESSUP"))
+      assert(lirc.get.codesForRemote("TestRemote1").size == 4)
+      assert(lirc.get.codesForRemote("TestRemote1").contains("KEY_BRIGHTNESSUP"))
     }
   }
 }
